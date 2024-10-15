2e8ef860b9e455c19640e0adf4ed5f28c552c6c3
Alex Lu <alexlinglu@gmail.com>
2024-10-15 17:13:31 -0400

    added css and js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 function level_10() {
+    document.getElementById("l10").innerHTML = "Hello, world!";
 }
 
 
 function level_11() {
+    if(!(document.getElementById("l11").style.backgroundColor == "yellow")){
+        document.getElementById("l11").style.backgroundColor = "yellow";
+        return;
+    }
+    document.getElementById("l11").style.backgroundColor = "";
 }
 
 function level_13() {
-    
+    rows = document.getElementById("l13_input").value;
+    output = ""
+    for(let j = 1; j <= rows; j++){
+        for(let i = 0; i < j; i++){
+            output += "*";
+        }
+        output += "<br>";
+    }
+    document.getElementById("l13_output").innerHTML = output;
 }
